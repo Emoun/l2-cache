8f3c335abd8006a9e67176f57806c8bbb79e0561
Arijus Grotuzas <arijusgrotuzas@gmail.com>
2025-10-07 15:01:50 +0200

    Remove old implementation

--- src/main/scala/caches/hardware/ocp/OcpBurst.scala
+++ src/main/scala/caches/hardware/ocp/OcpBurst.scala
@@ -7,12 +7,6 @@ class OcpBurstMasterSignals(addrWidth: Int, dataWidth: Int)
   extends OcpMasterSignals(addrWidth, dataWidth) {
   val DataValid = Output(UInt(1.W))
   val DataByteEn = Output(UInt((dataWidth / 8).W))
-
-  // This does not really clone, but Data.clone doesn't either
-  override def clone() = {
-    val res = new OcpBurstMasterSignals(addrWidth, dataWidth)
-    res.asInstanceOf[this.type]
-  }
 }
 
 // Burst slaves provide handshake signal
@@ -25,7 +19,6 @@ class OcpBurstSlaveSignals(dataWidth: Int)
 // Master port
 class OcpBurstMasterPort(addrWidth: Int, dataWidth: Int, burstLen: Int) extends Bundle() {
   val burstLength = burstLen
-  // Clk is implicit in Chisel
   val M = Output(new OcpBurstMasterSignals(addrWidth, dataWidth))
   val S = Input(new OcpBurstSlaveSignals(dataWidth))
 }
@@ -33,7 +26,6 @@ class OcpBurstMasterPort(addrWidth: Int, dataWidth: Int, burstLen: Int) extends 
 // Slave port is reverse of master port
 class OcpBurstSlavePort(val addrWidth: Int, val dataWidth: Int, val burstLen: Int) extends Bundle() {
   val burstLength = burstLen
-  // Clk is implicit in Chisel
   val M = Input(new OcpBurstMasterSignals(addrWidth, dataWidth))
   val S = Output(new OcpBurstSlaveSignals(dataWidth))
 }
